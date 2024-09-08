e385dc041af7a24e6c8389efe46d543a77ab9762
Rodrigo Freitas <164232597+rodrigosfreita@users.noreply.github.com>
2024-09-08 13:05:07 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,35 +1,36 @@
-package aula9_Metodos.CalculadoraSimples;
+package Methods.SimpleCalculator;
 
 import java.util.Scanner;
 
 public class App {
-	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		
-		CalculadoraSimples cs = new CalculadoraSimples();
-		
-		int op = cs.menu();
-		
-		System.out.println("N1: ");
-		float n1 = input.nextFloat();
-		System.out.println("N2: ");
-		float n2 = input.nextFloat();
-		
-		if(op == 1) {
-			float resultado = cs.somar(n1, n2);
-			System.out.println("Resultado: " + resultado);
-		}else if(op == 2) {
-			float resultado = cs.subtrair(n1, n2);
-			System.out.println("Resultado: " + resultado);
-		}else if(op == 3) {
-			float resultado = cs.multiplicar(n1, n2);
-			System.out.println("Resultado: " + resultado);
-		}else if(op == 4) {
-			float resultado = cs.dividir(n1, n2);
-			System.out.println("Resultado: " + resultado);
-		}else {
-			System.out.println("Operação inválida!");
-		}
-	}
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        SimpleCalculator sc = new SimpleCalculator();
+        
+        int op = sc.menu();
+        
+        System.out.println("N1: ");
+        float n1 = input.nextFloat();
+        System.out.println("N2: ");
+        float n2 = input.nextFloat();
+        
+        if(op == 1) {
+            float result = sc.add(n1, n2);
+            System.out.println("Result: " + result);
+        } else if(op == 2) {
+            float result = sc.subtract(n1, n2);
+            System.out.println("Result: " + result);
+        } else if(op == 3) {
+            float result = sc.multiply(n1, n2);
+            System.out.println("Result: " + result);
+        } else if(op == 4) {
+            float result = sc.divide(n1, n2);
+            System.out.println("Result: " + result);
+        } else {
+            System.out.println("Invalid operation!");
+        }
+    }
 }
+
